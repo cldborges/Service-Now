3bcca0ff9bef90dccf3f2145b2ca2a5b69f39bc8
cldborges <cldborges@yahoo.com.br>
2022-12-08 15:03:30 -0300

    browser aberto com user data personalizado e CDM

---
+++ main.py
@@ -0,0 +1,16 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from datetime import date
+
+
+url = 'https://siemens.service-now.com'
+options = webdriver.ChromeOptions()
+options.add_argument(r'--user-data-dir=C:/Temp/Sessoes/UserData/') #caso dê erros com o caminho, principalmente quando termina com \, alterar as barras para /
+driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager(path = r"C:\Temp\Sessoes\chromedriver").install()))
+driver.get(url)
+time.sleep(100)
