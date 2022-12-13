b79a73268e5f289e015a4d422f3692a874b1f3c7
cldborges <cldborges@yahoo.com.br>
2022-12-13 14:58:59 -0300

    todos os campos mapeados

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ from selenium.webdriver.support.ui import Select
 import time
 from datetime import date
 
+costumer_id = 'z0026jjc'
 category = 'Workplace'
 subcategory = 'DWP Win 10 Local Client'
 subsubcategory = 'System Performance'
+tipo = 'Service Request'
+short_description = 'Testando'
+description = '''Testando 123'''
+data_points = '''Testando 321'''
 
 
 url = 'https://siemens.service-now.com'
@@ -27,12 +32,13 @@ print(criar_novo.text)
 time.sleep(5)
 criar_novo.click()
 time.sleep(10)
+
 driver.switch_to.frame('gsft_main')
+
 WebDriverWait(driver, 20).until(
     EC.presence_of_element_located((By.XPATH, '//*[@id="label.incident.caller_id"]/label/span[2]'))) #//*[@id="sys_display.incident.caller_id"]
-costumer_id = driver.find_element(By.ID, 'sys_display.incident.caller_id')
-costumer_id.send_keys('z0026jjc')
-time.sleep(5)
+driver.find_element(By.ID, 'sys_display.incident.caller_id').send_keys(costumer_id)
+time.sleep(3)
 Select(driver.find_element(By.ID, 'incident.contact_type')).select_by_visible_text('Walk-in')
 try:
     WebDriverWait(driver, 15).until(
@@ -53,7 +59,28 @@ Select(driver.find_element(By.ID, 'sys_select.incident.u_subsubcategory')).selec
 
 
 
+time.sleep(3)
+driver.find_element(By.ID, 'sys_display.incident.assignment_group').clear()
+time.sleep(3)
+driver.find_element(By.ID, 'sys_display.incident.assignment_group').send_keys('INC_Atos_L1.5_UseIT_Center_BRAZIL')
 
+Select(driver.find_element(By.ID, 'incident.u_type')).select_by_visible_text(tipo)
 
+time.sleep(3)
+driver.find_element(By.ID, 'sys_display.incident.assigned_to').send_keys('BORGES CLAUDIO (IT DF SIAM STG)')
+
+driver.find_element(By.ID, 'incident.short_description').send_keys(short_description)
+driver.find_element(By.ID, 'incident.description').send_keys(description)
+
+try:
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.ID, 'incident.u_data_points')))
+
+    dp = driver.find_element(By.ID, 'incident.u_data_points').get_attribute('value')
+    # driver.find_element(By.ID, 'incident.u_data_points').send_keys(data_points)
+    # data_points = driver.find_element(By.ID, 'incident.u_data_points')
+    print(dp)
+except:
+    print('Não tem DP') 
 
 time.sleep(100)
